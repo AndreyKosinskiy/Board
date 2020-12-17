bcd0370bdd3fcf20b531cb2db459ba9067492371
metrolog <kosinskiy.andrey@ukr.net>
2020-12-17 02:23:52 +0200

    add footer

--- board.js
+++ board.js
@@ -1,13 +1,17 @@
 const listsSelector = ".list-ui"
 const $lists = $(listsSelector)
 
-optionsDraggable={
-    handle:'.list-ui-header'
-}
-$lists.draggable(optionsDraggable);
+// optionsDraggable={
+//     handle:'.list-ui-header'
+// }
+// $lists.draggable(optionsDraggable).disableSelection();
 
 optionsSortable ={
     connectWith: listsSelector,
     items:'.card-ui',
+    cursor:"grabbing",
+    forcePlaceholderSize: true,
+    forceHelperSize: true,
+    placeholder:'card-ui-placeholder'
 }
-$lists.sortable(optionsSortable);
+$lists.sortable(optionsSortable).disableSelection();;
