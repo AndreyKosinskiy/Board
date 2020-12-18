4eef645feb36ca03f1df60590e5adad2de16dd14
Andrey Kosinskiy <kosinskiyandrey@ukr.net>
2020-12-18 17:09:02 +0200

    delete selectable, add custom select functionality.

--- board.js
+++ board.js
@@ -1,97 +1,146 @@
-$(function(){
-    const boardSelector = ".board-ui"
-    const listsSelector = ".list-ui"
-    const listsSelectorBody = ".list-ui-body"
-    const cardsSelector = '.card-ui'
-    const zIndexDefault = 1000
-    const zIndexUp = zIndexDefault+50
+$(function() {
+	const boardSelector = '.board-ui';
+	const listsSelector = '.list-ui';
+	const listsSelectorBody = '.list-ui-body';
+	const cardsSelector = '.card-ui';
+	const zIndexDefault = 1000;
+	const zIndexUp = zIndexDefault + 50;
 
-    const $lists = $(listsSelector)
-    const $listsBody = $(listsSelectorBody)
-    const $cards = $(cardsSelector)
+	const $lists = $(listsSelector);
+	const $listsBody = $(listsSelectorBody);
+	const $cards = $(cardsSelector);
 
-     // Jquery Ui Resizable 
-    //  const startResize = function( event, ui ) {
-    //     let $item =  ui.element
-    //         $item.css('z-index', zIndexUp)
-
-    //  }
-    //  const stopResize = function( event, ui ) {
-    //     let $item =  ui.element
-    //     if($item){
-    //         $item.css('z-index',zIndexDefault)
-    //     }
-    //  }
-    //  const eventsResizable = {
-    //     start: startResize,
-    //     stop: stopResize
-    // }
-    // optionResizable = {
-    //     ...eventsResizable,
-    //     minHeight: 100,
-    //     minWidth: 100,
-    // }
-    // $lists.each(function(index,elem){
-    //     $list = $(elem)
-    //     const bodyList = $list.find(listsSelectorBody)
-    //     optionResizable['alsoResize'] = bodyList
-    //     $list.resizable(optionResizable).disableSelection();
-
-    // })
-
-    // Jquery Ui Draggable 
-
-    // optionsDraggable={
-    //     handle:'.list-ui-header',
-    //     containment: ".board-ui"
-    // }
-    // $lists.draggable(optionsDraggable).disableSelection();
-
-    // Jquery Ui Sortable
-    // const dragItemStart = function( event, ui ) {
-    //     let $item =  ui.item
-    //     $item.css('z-index', zIndexUp)
-    // }
-
-    // const dragItemStop = function( event, ui ) {
-    //     let $item =  ui.item
-    //     $item.css('z-index', zIndexDefault)
-    // }
-
-    // const eventsSortable = {
-    //     start: dragItemStart,
-    //     stop:dragItemStop
-    // }
-
-    // optionsSortable ={
-    //     ...eventsSortable,
-    //     connectWith: listsSelectorBody,
-    //     items:cardsSelector,
-    //     cursor:"grabbing",
-    //     forcePlaceholderSize: true,
-    //     forceHelperSize: true,
-    //     placeholder:'card-ui-placeholder'
-    // }
-    // $listsBody.sortable(optionsSortable).disableSelection();
-
-    // Jqury Ui Selectable
-    const selectItemStart =  function( event, ui ) {
-        console.log(event);
-        console.log(ui);
-        console.log(event.target.value)
+    const elementDown = function(event, ui) {
+		let $item = ui.element || $(ui.helper);
+        $item.css('z-index', zIndexDefault);
     }
-    eventsSelectable = {
-        start:selectItemStart
-    }
-    optionsSelectable = {
-        ...eventsSelectable,
-        tolerance: "touch",
-        classes: {
-            "ui-selected": "card-ui-selected",
-            "ui-selecting":"card-ui-selected"
-          }
-    }
-    $cards.selectable(optionsSelectable);
-})
+    const elementUp = function(event, ui) {
+		let $item = ui.element || ui.item;
+		$item.css('z-index', zIndexUp);
+    };
 
- 
+	// Jquery Ui Resizable
+
+	const startResize = function(event, ui) {
+        elementUp(event, ui)
+	};
+	const stopResize = function(event, ui) {
+		elementDown(event, ui)
+	};
+	const eventsResizable = {
+		start: startResize,
+		stop: stopResize
+	};
+	optionResizable = {
+		...eventsResizable,
+		minHeight: 100,
+		minWidth: 100
+	};
+	$lists.each(function(index, elem) {
+		$list = $(elem);
+		const bodyList = $list.find(listsSelectorBody);
+		optionResizable['alsoResize'] = bodyList;
+		$list.resizable(optionResizable).disableSelection();
+	});
+
+	// Jquery Ui Draggable
+	// Drag lists on the board
+
+    const startDraggable = function( event, ui ) {
+		
+    }
+    const stopDraggable = function(event, ui){
+        
+	}
+	const dragDraggable = function( event, ui ) {
+		
+	}
+    const eventsDraggable ={
+		start: startDraggable,
+		drag: dragDraggable,
+        stop: stopDraggable,
+    }
+	optionsDraggable = {
+        ...eventsDraggable,
+		handle: '.list-ui-header',
+		containment: boardSelector,
+		stack: listsSelector
+	};
+	$lists.draggable(optionsDraggable).disableSelection();
+
+    //Jquery Ui Sortable
+	// Drag cards on the board and put to lists
+
+   
+
+	const dragItemStop = function(event, ui) {
+		elementDown(event, ui)
+		const selected = ui.item.data('multidrag');
+		ui.item.after(selected);
+		selected.css('z-index', zIndexDefault)
+      	ui.item.remove();
+	};
+
+	const multiHelper = function(e, item) {
+		if ( ! item.hasClass('ui-selected') ) {
+		  item.parent().children('.ui-selected').removeClass('ui-selected');
+		  item.addClass('ui-selected');
+		}
+  		var selected = item.parent().children('.ui-selected').clone();
+		item.data('multidrag', selected).siblings('.ui-selected').remove();
+		return $('<div/>').append(selected);
+	  }
+
+	const eventsSortable = {
+		stop: dragItemStop
+	};
+
+	optionsSortable = {
+		...eventsSortable,
+		connectWith: listsSelectorBody,
+		items: cardsSelector,
+		cursor: 'grabbing',
+		forcePlaceholderSize: true,
+		forceHelperSize: true,
+		placeholder: 'card-ui-placeholder',
+		appendTo: 'body',
+		helper:multiHelper
+	};
+
+	$listsBody.sortable(optionsSortable).disableSelection()
+
+	$('body').on('click','.card-ui',function(event){
+		$(event.target).toggleClass('ui-selected')
+		console.log('click')
+	})
+
+	// // Jqury Ui Selectable	
+    // const selectCreate = function( event, ui ) {
+	// 	return false;
+	// }
+	// const selectItemStart = function(event, ui) {
+
+	// 	console.log('selectItemStart')
+	
+	// };
+	// const selectingItem = function( event, ui ) {
+	// 	console.log(ui)
+	// }
+	// eventsSelectable = {
+	// 	create: selectCreate,
+	// 	start: selectItemStart,
+	// 	selecting:selectingItem
+	// };
+	// optionsSelectable = {
+	// 	...eventsSelectable
+	// 	// classes: {
+	// 	// 	'ui-selected': 'card-ui-selected',
+	// 	// 	'ui-selecting': 'card-ui-selected'
+	// 	// }
+	// };
+    // $listsBody.each(function(index,elem){
+	// 	let $listBody = $(elem)
+	// 	optionsSelectable['appendTo'] = elem
+	// 	$listBody.selectable(optionsSelectable)
+	// })
+});
